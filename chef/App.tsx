9719f6af02932b1e7470e293d77cb8922ae38d59
Bibhav48 <76898850+Bibhav48@users.noreply.github.com>
2024-05-04 20:04:00 +0545

    feat: basic dark mode

--- chef/App.tsx
+++ chef/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text, Pressable} from "react-native";
+import { useColorScheme } from "nativewind";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const { colorScheme, setColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 bg-slate-900 items-center justify-center">
-      <Text className="text-white text-center text-4xl font-bold">
+    <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
+      <Pressable
+        className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
+        onPress={() =>
+          setColorScheme("dark" === colorScheme ? "light" : "dark")
+        }
+        accessibilityLabel="Learn more about this purple button"
+      >
+        <Text className="text-white">
+          {"dark" === colorScheme ? "🌞" : "🌙"}
+        </Text>
+      </Pressable>
+      <Text className="text-black dark:text-white text-4xl font-bold">
         Ultifood
       </Text>
-      <Text className="text-white text-center text-lg">Subtitle</Text>
-    </View>
+      <Text className="dark:text-white dark:text-center text-lg">subtitle</Text>
+    </SafeAreaView>
   );
 }
