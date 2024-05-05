0c94831e2d0159284c4aa74e2c076dd999df32d6
Bibhav48 <76898850+Bibhav48@users.noreply.github.com>
2024-05-05 09:55:08 +0545

    fix: dark-mode

--- chef/App.tsx
+++ chef/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { View, Text, Pressable} from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useColorScheme } from "nativewind";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const { colorScheme, setColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 bg-slate-900 items-center justify-center">
-    <Text className="text-white text-center text-4xl font-bold"></Text>
     <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
       <Pressable
         className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
@@ -23,9 +21,7 @@ export default function App() {
       <Text className="text-black dark:text-white text-4xl font-bold">
         Ultifood
       </Text>
-      <Text className="text-white text-center text-lg">Subtitle</Text>
-      </SafeAreaView>
-      <Text className="dark:text-white dark:text-center text-lg">subtitle</Text>
-    </View>
+      <Text className="text-black dark:text-white text-center text-lg">Subtitle</Text>
+    </SafeAreaView>
   );
 }
