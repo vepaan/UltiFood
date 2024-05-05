966e23454fa00aea2cc4f4319f726e75acfe4e87
Bibhav48 <76898850+Bibhav48@users.noreply.github.com>
2024-05-05 12:47:29 +0545

    feat: nav-hidden

--- chef/App.tsx
+++ chef/App.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header, Loading, ThemeToggle } from "./components/home";
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const setVisibility = async (visibility:'visible' | 'hidden') => {
+      await NavigationBar.setVisibilityAsync(visibility)
+    };
+    setVisibility('hidden');
     const timer = setTimeout(() => {
       setLoading(false);
+      setVisibility('visible');
     }, 6000);
     return () => clearTimeout(timer);
   }, []);
