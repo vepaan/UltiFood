a0c398040871f8c89f36f292c119690f12e6bdb6
vepaan <141347566+vepaan@users.noreply.github.com>
2024-05-05 08:28:00 +0545

    syntax edit

--- chef/App.tsx
+++ chef/App.tsx
@@ -7,10 +7,25 @@ export default function App() {
   const { colorScheme, setColorScheme } = useColorScheme();
   return (
     <View className="flex-1 bg-slate-900 items-center justify-center">
-      <Text className="text-white text-center text-4xl font-bold">
+    <Text className="text-white text-center text-4xl font-bold"></Text>
+    <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
+      <Pressable
+        className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
+        onPress={() =>
+          setColorScheme("dark" === colorScheme ? "light" : "dark")
+        }
+        accessibilityLabel="Learn more about this purple button"
+      >
+        <Text className="text-white">
+          {"dark" === colorScheme ? "🌞" : "🌙"}
+        </Text>
+      </Pressable>
+      <Text className="text-black dark:text-white text-4xl font-bold">
         Ultifood
       </Text>
+      <Text className="text-white text-center text-lg">Subtitle</Text>
+      </SafeAreaView>
       <Text className="dark:text-white dark:text-center text-lg">subtitle</Text>
-    </SafeAreaView>
+    </View>
   );
 }
