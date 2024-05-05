c18be7065ade0e822763b60e9a09dcde1dd911f0
Bibhav48 <76898850+Bibhav48@users.noreply.github.com>
2024-05-05 12:37:19 +0545

    refactor animation

--- chef/App.tsx
+++ chef/App.tsx
@@ -1,47 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, Image, Pressable, Animated, Easing } from "react-native";
-import { useColorScheme } from "nativewind";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Header, Loading, ThemeToggle } from "./components/home";
-import styles from './AppStyles';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const spinValue = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);}, 6000);
-      return () => clearTimeout(timer);}, []);
-
-  useEffect(() => {
-    if (loading) {
-      startSpinAnimation();
-    }
+      setLoading(false);
+    }, 6000);
+    return () => clearTimeout(timer);
   }, []);
 
-  const startSpinAnimation = () => {
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(spinValue, {toValue: 1, duration: 1500, easing: Easing.linear, useNativeDriver: true,}),
-        Animated.delay(500),
-        Animated.timing(spinValue, {toValue: 0, duration: 0, useNativeDriver: true,})
-      ])
-    ).start();
-  };
-
-  const spin = spinValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ["0deg", "720deg"],
-  });
-
   return loading ? (
-    <View style={{ flex: 1, backgroundColor: "#ffaa59", alignItems: "center", justifyContent: "center" }}>
-      <Animated.Image
-        source={require("./assets/logo.png")}
-        style={[styles.logo, { transform: [{ translateX: -0}, {translateY: -0}, {rotate: spin}, {translateX: 10}, {translateY: 0}] }]}
-      />
-    </View>
+    <Loading />
   ) : (
     <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
       <ThemeToggle />
--- chef/components/home.tsx
+++ chef/components/home.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Text, Pressable, Animated, Easing } from "react-native";
 import { useColorScheme } from "nativewind";
-import styles from '../AppStyles';
+import styles from "../AppStyles";
 
 function Header() {
   return (
@@ -16,28 +16,72 @@ function Header() {
   );
 }
 
-function Loading(){
-    return (
-        <View className="flex-1 bg-[#ffaa59] items-center justify-center">
-            <Image source={require("../assets/logo.png")} style={styles.logo} />
-        </View>
-    )
+function Loading() {
+  const spinValue = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    startSpinAnimation();
+  }, []);
+  const startSpinAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(spinValue, {
+          toValue: 1,
+          duration: 1500,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.delay(500),
+        Animated.timing(spinValue, {
+          toValue: 0,
+          duration: 0,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  };
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "720deg"],
+  });
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#ffaa59",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Animated.Image
+        source={require("../assets/logo.png")}
+        style={[
+          styles.logo,
+          {
+            transform: [
+              { translateX: -0 },
+              { translateY: -0 },
+              { rotate: spin },
+              { translateX: 10 },
+              { translateY: 0 },
+            ],
+          },
+        ]}
+      />
+    </View>
+  );
 }
 
-function ThemeToggle(){
-const { colorScheme, setColorScheme } = useColorScheme();
+function ThemeToggle() {
+  const { colorScheme, setColorScheme } = useColorScheme();
   return (
     <Pressable
-    className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
-    onPress={() =>
-      setColorScheme("dark" === colorScheme ? "light" : "dark")
-    }
-    accessibilityLabel="Learn more about this purple button"
-  >
-    <Text className="text-white">
-      {"dark" === colorScheme ? "🌞" : "🌙"}
-    </Text>
-  </Pressable>
+      className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
+      onPress={() => setColorScheme("dark" === colorScheme ? "light" : "dark")}
+      accessibilityLabel="Learn more about this purple button"
+    >
+      <Text className="text-white">{"dark" === colorScheme ? "🌞" : "🌙"}</Text>
+    </Pressable>
   );
 }
 export { Header, Loading, ThemeToggle };
