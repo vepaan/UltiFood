8babfff4c3b06e5049eecf090f4100978c86f8ac
vepaan <141347566+vepaan@users.noreply.github.com>
2024-05-05 14:04:31 +0545

    fuckinghell

--- chef/App.tsx
+++ chef/App.tsx
@@ -5,27 +5,45 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Header, Loading, ThemeToggle } from "./components/home";
 import styles from './AppStyles';
 
+
+const textArray = ['Burning Calories', 'Synthesizing Proteins', 'Building Muscle']
+
 export default function App() {
+  const { colorScheme, setColorScheme } = useColorScheme();
   const [loading, setLoading] = useState(true);
   const spinValue = useRef(new Animated.Value(0)).current;
+  const [displayText, setDisplayText] = useState('');
+  const [dots, setDots] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);}, 6000);
-      return () => clearTimeout(timer);}, []);
+      setLoading(loading+1);});
+      return () => clearTimeout(timer);}, [loading]);
 
   useEffect(() => {
     if (loading) {
       startSpinAnimation();
     }
-  }, []);
+  }, []); // Re-trigger effect when loading state changes
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentIndex = Math.floor((new Date().getTime() / 1000) % textArray.length);
+      const text = textArray[currentIndex];
+      setDisplayText(text);
+    }, 1000); // Update text every 1 second
+  
+    return () => clearInterval(interval);
+  }, []); // Run once on component mount
+  
 
   const startSpinAnimation = () => {
     Animated.loop(
       Animated.sequence([
-        Animated.timing(spinValue, {toValue: 1, duration: 1500, easing: Easing.linear, useNativeDriver: true,}),
-        Animated.delay(500),
-        Animated.timing(spinValue, {toValue: 0, duration: 0, useNativeDriver: true,})
+        Animated.timing(spinValue, { toValue: 1, duration: 1400, easing: Easing.linear, useNativeDriver: true, }),
+        Animated.delay(900),
+        Animated.timing(spinValue, { toValue: 0, duration: 0, useNativeDriver: true, })
       ])
     ).start();
   };
@@ -39,8 +57,10 @@ export default function App() {
     <View style={{ flex: 1, backgroundColor: "#ffaa59", alignItems: "center", justifyContent: "center" }}>
       <Animated.Image
         source={require("./assets/logo.png")}
-        style={[styles.logo, { transform: [{ translateX: -0}, {translateY: -0}, {rotate: spin}, {translateX: 10}, {translateY: 0}] }]}
+        style={[styles.logo, { transform: [{ translateX: -10 }, { translateY: -0 }, { rotate: spin }, { translateX: 10 }, { translateY: 0 }] }]}
       />
+      <Image source={require("./assets/logotext.png")} style={styles.logotext}></Image>
+      <Text style={{ color: '#000', fontSize: 30, fontFamily:'sans', marginTop: 20 }}>{displayText}{dots}</Text>
     </View>
   ) : (
     <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
--- chef/AppStyles.ts
+++ chef/AppStyles.ts
@@ -2,14 +2,21 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     flex: 1,
     backgroundColor: '#0f172a',
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
-    width: 200,
-    height: 200,
+    position: 'absolute',
+    width: '55%',
+    resizeMode: 'contain',
+  },
+  logotext: {
+    top: '17%',
+    position: 'absolute',
+    width: '60%',
     resizeMode: 'contain',
   }
 });
