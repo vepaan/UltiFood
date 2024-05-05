378ac587385f7523f5ecb3dbaa6bf0ce790d9215
vepaan <141347566+vepaan@users.noreply.github.com>
2024-05-05 08:20:02 +0545

    hii

--- chef/App.tsx
+++ chef/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 bg-slate-900 items-center justify-center">
       <Text className="text-white text-center text-4xl font-bold">
-        Ultifood
+        UltiFood
       </Text>
       <Text className="text-white text-center text-lg">Subtitle</Text>
     </View>
