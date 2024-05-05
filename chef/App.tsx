66880beb8678860204405dcb355b7e6ad2af9e33
vepaan <141347566+vepaan@users.noreply.github.com>
2024-05-05 11:48:21 +0545

    spinning

--- chef/App.tsx
+++ chef/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, Image, Pressable, Animated, Easing } from "react-native";
 import { useColorScheme } from "nativewind";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './AppStyles';
@@ -7,19 +7,43 @@ import styles from './AppStyles';
 export default function App() {
   const { colorScheme, setColorScheme } = useColorScheme();
   const [loading, setLoading] = useState(true);
+  const spinValue = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    // Hide logo and start other shit after 6 seconds
     const timer = setTimeout(() => {
       setLoading(false);}, 6000);
-      return () => clearTimeout(timer);}, []); // Run only once on component mount. No dependecy array for now
+      return () => clearTimeout(timer);}, []);
 
-  return loading ? ( // Display the logo only when loading is true
-    <View className="flex-1 bg-[#ffaa59] items-center justify-center">
-      <Image source={require("./assets/logo.png")} style={styles.logo} />
+  useEffect(() => {
+    if (loading) {
+      startSpinAnimation();
+    }
+  }, []);
+
+  const startSpinAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(spinValue, {toValue: 1, duration: 1500, easing: Easing.linear, useNativeDriver: true,}),
+        Animated.delay(500),
+        Animated.timing(spinValue, {toValue: 0, duration: 0, useNativeDriver: true,})
+      ])
+    ).start();
+  };
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return loading ? (
+    <View style={{ flex: 1, backgroundColor: "#ffaa59", alignItems: "center", justifyContent: "center" }}>
+      <Animated.Image
+        source={require("./assets/logo.png")}
+        style={[styles.logo, { transform: [{ translateX: -0}, {translateY: -0}, {rotate: spin}, {translateX: 10}, {translateY: 0}] }]}
+      />
     </View>
   ) : (
-    <SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
+<SafeAreaView className="flex-1 bg-gray-300 dark:bg-gray-900 items-center justify-center">
       <Pressable
         className="bg-gray-400 absolute top-16 right-8 dark:bg-blue-950 font-bold py-2 px-4 rounded-xl"
         onPress={() =>
